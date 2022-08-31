de15e17158d516dadbd366c05ce6dd517d5fc777
Damian Dorosz <doroszdam@gmail.com>
2022-08-31 12:34:07 +0200

    2021: add Day1 solution

--- AdventOfCode.Solutions/Year2021/Day01/Solution.cs
+++ AdventOfCode.Solutions/Year2021/Day01/Solution.cs
@@ -6,11 +6,78 @@ class Solution : SolutionBase
 
     protected override string SolvePartOne()
     {
-        return "";
+        string[] depthReadings = Input.SplitByNewline();
+        int[] depthMeasurements = new int[depthReadings.Length];
+            
+        ConvertReadingsToIntMeasurements(depthReadings, depthMeasurements);
+
+        int cachedDepth = depthMeasurements[0];
+            
+        int depthIncreasedCount = 0;
+
+        int iterationsCount = depthReadings.Length;
+            
+        for (int i = 1; i < iterationsCount; i++)
+        {
+            int currentDepth = depthMeasurements[i];
+            if (currentDepth > cachedDepth) depthIncreasedCount++;
+            cachedDepth = currentDepth;
+        }
+        return depthIncreasedCount.ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        string[] depthReadings = Input.SplitByNewline();
+        int[] depthMeasurements = new int[depthReadings.Length];
+            
+        ConvertReadingsToIntMeasurements(depthReadings, depthMeasurements);
+
+        int cachedDepthSum =
+            new MeasurementWindow(depthMeasurements[0], depthMeasurements[1], depthMeasurements[2])
+                .MeasurementSum();
+            
+        int depthIncreasedCount = 0;
+            
+        int iterationsCount = depthReadings.Length - depthReadings.Length % 3;
+            
+        for (int i = 1; i < iterationsCount; i++)
+        {
+            int currentDepthSum = 
+                new MeasurementWindow(depthMeasurements[i], depthMeasurements[i + 1], depthMeasurements[i + 2])
+                    .MeasurementSum();
+            if (currentDepthSum > cachedDepthSum) depthIncreasedCount++;
+            cachedDepthSum = currentDepthSum;
+        }
+        return depthIncreasedCount.ToString();
     }
+
+    #region PartTwo
+    private static void ConvertReadingsToIntMeasurements(string[] depthReadings, int[] depthMeasurements)
+    {
+        for (int i = 0; i < depthReadings.Length; i++)
+        {
+            depthMeasurements[i] = int.Parse(depthReadings[i]);
+        }
+    }
+
+    private class MeasurementWindow
+    {
+        private int MeasurementOne {get; set;}
+        private int MeasurementTwo {get; set;}
+        private int MeasurementThree {get; set;}
+        
+        public MeasurementWindow(int firstMeasure, int secondMeasure, int thirdMeasure)
+        {
+            MeasurementOne = firstMeasure;
+            MeasurementTwo = secondMeasure;
+            MeasurementThree = thirdMeasure;
+        }
+        
+        public int MeasurementSum()
+        {
+            return MeasurementOne + MeasurementTwo + MeasurementThree;
+        }
+    }
+    #endregion
 }
